eca3572a3de95d866d22fbf53fb856a2d8801e42
Davit Tabidze <davidtabidzef1@gmail.com>
2023-05-23 21:45:18 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching (drivers,matchName) {
+    return drivers.filter(name => name.toLowerCase() === matchName.toLowerCase());
+}
+
+function fuzzyMatch (drivers,letters) {
+    return drivers.filter(name => name.slice(0,letters.length) === letters);
+}
+
+function matchName(drivers,matchName) {
+    return drivers.filter(driver => driver.name === matchName);
+}
